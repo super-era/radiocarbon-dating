433258c83f0dbdca74c2c76dde71df9c51b2657a
super-era <79094444+super-era@users.noreply.github.com>
2021-05-16 21:38:27 +1000

    Update radiocarbondating.py

--- radiocarbondating.py
+++ radiocarbondating.py
@@ -23,31 +23,35 @@ def main():
         # setting up the program exit method with an if/else statement
         if remaining_carbon == 'x':
             break
+
         # setting up the branch where the user input is used to calculate the sample's age
-        else:
-            remaining_carbon = float(remaining_carbon)
-
-        # decimal conversion
-        remaining_carbon = remaining_carbon / 100
-
-        # c14 decay formula solved for time in years: 
-        # more info here http://www.biology.arizona.edu/biomath/tutorials/applications/CarbonQ4t.html
-        # and here http://www.biology.arizona.edu/biomath/tutorials/applications/carbon.html
-        time_in_years = (math.log(remaining_carbon)) / k
+        time_in_years = calculate_sample_age(float(remaining_carbon))
 
         # rounding the number of years to one decimal place as learnt from tutor Shai! #Section116
         time_in_years = round(time_in_years, 1)
 
-        # calculating the year in CE that the sample would have come from
-        common_era_year = CURRENT_YEAR - int(time_in_years)
-
-        # converting to BCE if needed
-        if common_era_year < 0:
-            str_common_era_year = str(-common_era_year) + " BCE"
-        else:
-            str_common_era_year = str(common_era_year) + " CE"
+        str_common_era_year = calculate_age_in_CE(time_in_years)
 
         # return answer to user
         print("Your sample is around " + str(time_in_years) + " years old.")
         print("That means your sample is from around " + str_common_era_year + "!")
         print()   # blank line between program answers for clarity
+
+def calculate_sample_age(remaining_carbon):
+    # c14 decay formula solved for time in years: 
+    # more info here http://www.biology.arizona.edu/biomath/tutorials/applications/CarbonQ4t.html
+    # and here http://www.biology.arizona.edu/biomath/tutorials/applications/carbon.html
+    return math.log(remaining_carbon/100)/k
+
+def calculate_age_in_CE(time_in_years):
+    # calculating the year in CE that the sample would have come from, and converting to BCE if needed
+    common_era_year = CURRENT_YEAR - int(time_in_years)
+    if common_era_year < 0:
+        str_common_era_year = str(-common_era_year) + " BCE"
+    else:
+        str_common_era_year = str(common_era_year - 1) + " CE"      # minus 1 because there is no 0CE
+    return str_common_era_year
+
+
+if __name__ == '__main__':
+    main()
